9ae85c68d9a44d2136a6e54fd038aae19ab95c1d
Enric Pou <enricpou@gmail.com>
2025-05-20 13:34:41 +0200

    Added JWT interceptor

---
+++ interceptors.ts
@@ -0,0 +1,95 @@
+import { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
+import { AuthApi, AuthApiRefreshTokenRequest } from "./api";
+import { Configuration } from "./configuration";
+import { setBearerAuthToObject } from "./common";
+
+export function attachAuthInterceptor(
+  instance: AxiosInstance,
+  options: {
+    configuration: Configuration;
+    getRefreshToken: () => string | null;
+    updateAccessToken: (accessToken: string) => void;
+  }
+) {
+  const authApi = new AuthApi(options.configuration);
+  let isRefreshing = false;
+  let failedQueue: {
+    resolve: (token: string) => void;
+    reject: (err: any) => void;
+  }[] = [];
+
+  const processQueue = (error: any, token: string | null = null) => {
+    failedQueue.forEach((prom) => {
+      if (error) {
+        prom.reject(error);
+      } else {
+        prom.resolve(token!);
+      }
+    });
+    failedQueue = [];
+  };
+
+  // The Bearer token is already set on the request functions.
+  //   instance.interceptors.request.use((config) => {
+  //     return (async () => {
+  //       await setBearerAuthToObject(config, options.configuration);
+  //       return config;
+  //     })();
+  //   });
+
+  instance.interceptors.response.use(
+    (res) => res,
+    async (err: AxiosError) => {
+      if (!err.response) {
+        return Promise.reject(err); // Reject network errors
+      }
+      const originalRequest = err.config as AxiosRequestConfig & {
+        _retry?: boolean;
+      };
+
+      if (err.response?.status === 401 && !originalRequest._retry) {
+        if (isRefreshing) {
+          return new Promise((resolve, reject) => {
+            failedQueue.push({ resolve, reject });
+          }).then(async (token) => {
+            await setBearerAuthToObject(originalRequest, options.configuration);
+            return instance(originalRequest);
+          });
+        }
+
+        originalRequest._retry = true;
+        isRefreshing = true;
+
+        try {
+          const refreshToken = options.getRefreshToken();
+
+          if (!refreshToken) {
+            throw new Error("No refresh token available");
+          }
+
+          const refreshResponse = await authApi.refreshToken({
+            tokenRefreshRequest: {
+              refresh: refreshToken,
+            },
+          } as AuthApiRefreshTokenRequest);
+
+          const newToken = refreshResponse.data.access;
+          options.updateAccessToken(newToken);
+
+          processQueue(null, newToken);
+
+          await setBearerAuthToObject(originalRequest, options.configuration);
+
+          return instance(originalRequest);
+        } catch (refreshErr) {
+          processQueue(refreshErr, null);
+          return Promise.reject(refreshErr);
+        } finally {
+          isRefreshing = false;
+        }
+      }
+
+      return Promise.reject(err);
+    }
+  );
+}
